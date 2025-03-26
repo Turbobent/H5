473aa62d3c5fda8aefe44d10e8a9c059610aece9
Turbobent <kasp728h@edu.mercantec.dk>
2025-03-26 12:37:01 +0100

    error msg on logcontroller

--- API/Models/Log.cs
+++ API/Models/Log.cs
@@ -14,24 +14,24 @@ public class PostLog
 {
     public int DeviceId { get; set; }
 
-    public DateObject Date { get; set; }
-    public DateObject EndDate { get; set; }
+    public DatePart Date { get; set; }
+    public DatePart EndDate { get; set; }
 
-    public TimeObject ArmedTime { get; set; }
-    public TimeObject DisarmedTime { get; set; }
+    public TimePart ArmedTime { get; set; }
+    public TimePart DisarmedTime { get; set; }
 
     public bool IsTriggered { get; set; }
-    public TimeObject? TriggeredTime { get; set; }
+    public TimePart? TriggeredTime { get; set; }
 }
 
-public class DateObject
+public class DatePart
 {
     public int Year { get; set; }
     public int Month { get; set; }
     public int Day { get; set; }
 }
 
-public class TimeObject
+public class TimePart
 {
     public int Hour { get; set; }
     public int Minute { get; set; }
