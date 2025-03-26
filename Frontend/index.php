f6158210088056805be82a5620aba55d3eb9058e
chrisdk88 <108925053+chrisdk88@users.noreply.github.com>
2025-03-26 10:59:22 +0100

    Add frontend

---
+++ Frontend/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <?php include_once('templates/links.php') ?>
+  <title>Sentinel</title>
+</head>
+
+<body class="bg-[#6CD9D9]">
+  <!-- Header -->
+  <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/H5/Frontend/templates/header.php"); ?>
+
+  <section>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:min-h-[calc(100vh-140px)] lg:py-0">
+
+      <!-- Main -->
+      <h1 class="font-bold">Hello World!</h1>
+
+    </div>
+  </section>
+  <!-- Footer -->
+  <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/H5/Frontend/templates/footer.php"); ?>
+</body>
+
+</html>
---
+++ Frontend/templates/footer.php
@@ -0,0 +1,28 @@
+<!-- Include our links.php -->
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . "/H5/Frontend/templates/links.php"); ?>
+
+<footer class="bg-[#282828]">
+  <div
+    class="mx-auto flex flex-col md:flex-row h-[70px] max-w-screen-xl items-center justify-between gap-8 px-4 sm:px-6 lg:px-8">
+
+    <!-- Copyright Text -->
+    <div class="text-center md:text-left w-full md:w-auto">
+      <span class="text-sm text-gray-500 dark:text-gray-400">
+        © 2023 <a href="<?= $baseURL; ?>" class="hover:underline">Sentinel™</a>. All Rights Reserved.
+      </span>
+    </div>
+
+    <!-- Footer Navigation -->
+    <nav aria-label="Footer Navigation">
+      <ul class="flex items-center gap-6 text-sm">
+        <li>
+          <a class="text-gray-500 transition hover:text-gray-500/75" href="#">About</a>
+        </li>
+        <li>
+          <a class="text-gray-500 transition hover:text-gray-500/75" href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+  </div>
+</footer>
